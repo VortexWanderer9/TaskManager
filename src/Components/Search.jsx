a68bfae4f11d92601a91cd053faa11d5d1fb7255
Tyrell Wellick <keshavniure99@gmail.com>
2026-04-21 06:51:55 -0800

    Refactor Search and TaskList components: remove unused variables, enhance styling, and improve task display logic

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,7 +4,7 @@ import { useTask } from '../Context/TaskContextProvider';
 
 function Search() {
       const [text, setText] = useState('');
-      const {addTask, tasks} = useTask();
+      const {addTask} = useTask();
       const handleClick = () =>{
         if(text.trim().length <= 0){
             return
@@ -15,18 +15,18 @@ function Search() {
       }
   return (
     <div className='flex gap-3'>
-        <div> 
+        <div className='w-full'> 
             <input 
             type="text"
              placeholder='Enter your task' 
-             className='border rounded px-1 outline-none' 
+             className='border rounded px-1 outline-none w-full py-1' 
              onChange={(e) => setText(e.target.value)}
              value={text}
              />
         </div>
         <div>
             <button 
-            className='text-xl font-bold bg-blue-800 rounded px-2 cursor-pointer text-white hover:bg-blue-500 duration-200 ease-in-out'
+            className='text-xl font-bold bg-blue-800 py-1 w-25 rounded px-2 cursor-pointer text-white hover:bg-blue-500 duration-200 ease-in-out'
             onClick={handleClick}
             >Add
             </button>
--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -2,23 +2,31 @@ import { useTask } from '../Context/TaskContextProvider'
 import Search from './Search'
 
 function TaskList() {
-  const {tasks}  = useTask()
+  const {tasks, toggleTask, deleteTask}  = useTask()
+  const handleClick = (id) =>{
+    toggleTask(id)
+  }
+  const deleteT = (id) => {
+deleteTask(id)
+  }
   return (
     <>
   <div className=' mt-2 p-3 flex flex-col items-center'>
-<div>
+<div className='w-full'>
   <Search />
   <div>
     <h2 className='text-2xl mt-2 font-bold '>Tasks:</h2>
 <div className='flex flex-col gap-3'>
-  {tasks && tasks.map((t) => (
-    <div key={t.id} className='flex justify-between gap-2 items-center p-2 border-2 rounded border-gray-700/20 relative'>
+    {tasks && tasks.map((t) => (
+    <div key={t.id} className={`flex justify-between gap-2 items-center p-2 border-2 rounded border-gray-700/20 bg-blue-400 relative`}>
     <div>
-      <p>{t.text}</p>
+      <p className={t.isCompleted ? 'font-bold text-white line-through' : 'font-stretch-50% text-red-900'}>{t.text}</p>
     </div>
    <div className='flex gap-2'>
-     <img src={t.isCompleted ? "cross.svg" : "tick.svg"} width={20} alt="Status" />
-     <img src="trash.svg" width={15} className='absolute -top-2 -right-2 bg-gray-700/10 cursor-pointer' alt="Delete" />
+     <div className='cursor-pointer' onClick={(e) => handleClick(t.id)}>
+      <img src={t.isCompleted ? "cross.svg" : "tick.svg"} width={20} alt="Status" />
+     </div>
+      <img src="trash.svg" width={15} className='absolute -top-2 -right-1 bg-fuchsia-700 rounded  cursor-pointer' onClick={(e) => deleteT(t.id)} alt="Delete" />
    </div>
   </div>
   ))}
